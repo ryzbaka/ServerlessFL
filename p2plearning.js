bea770818d1dae0d6b1b97094bcf4b48992339bb
ryzbaka <hamzaali151214@hotmail.com>
2022-09-11 01:01:18 +0530

    implemented dh

--- p2plearning.js
+++ p2plearning.js
@@ -48,6 +48,8 @@ class PeerNode{
         this.peer.connections[id][0].send(JSON.stringify(data)) 
     }
     arrayStringToArray(s){
+        console.log("in arrayStringToArray")
+        console.log(s)
         const splitString = s.split(",")
         const arr = []
         splitString.forEach(el=>{
@@ -56,10 +58,12 @@ class PeerNode{
         return arr
     }
     handleData(connection,data){
-        showMessage(`Received from ${connection.peer}: ${data}`)
+        showMessage(`Received from ${connection.peer}: ${JSON.parse(data).message_type}`)
         if(this.locked){
             showError(`Did not respond to message: ${data} from ${connection.peer} (${this.peer.id} is locked.)`)
         }else{
+            this.locked = true
+            showMessage(`Locked ${this.peer.id}`)
             const messageObject = JSON.parse(data)
             if(messageObject.message_type==="join-fed"){
                 //check address book for peer with id already exists
@@ -76,13 +80,15 @@ class PeerNode{
                     showMessage("Generating key, prime and generator")
                     const keys = this.addressBook[connection.peer]["dhObject"].generateKeys()
                     showMessage("Generated key")
+                    console.log("my key is:")
+                    console.log(keys)
                     const prime = this.addressBook[connection.peer]["dhObject"].getPrime().toJSON().data
                     showMessage("Generated prime")
                     const generator = this.addressBook[connection.peer]["dhObject"].getGenerator().toJSON().data
                     showMessage("Generated generator")
                     showMessage(`Sending key, prime and generator to ${connection.peer}`)
                     this.sendMessage(connection.peer,{
-                        "message-type":"join-fed-init-dh",
+                        "message_type":"join-fed-init-dh",
                         "keys":keys.toJSON().data,
                         "prime":prime,
                         "generator":generator
@@ -90,18 +96,52 @@ class PeerNode{
                 }
             }else if(messageObject.message_type==="join-fed-init-dh"){
                 //IMPLEMENTING
+                // const otherKeys = this.arrayStringToArray(messageObject.keys)
                 const otherKeys = messageObject.keys
-                const prime = this.arrayStringToArray(messageObject.prime)
-                const generator = this.arrayStringToArray(messageObject.generator)
-                console.log(`Other key ${otherKeys}`)
-                console.log(`prime ${prime}`)
-                console.log(`generator ${generator}`)
+                // const prime = this.arrayStringToArray(messageObject.prime)
+                const prime = messageObject.prime
+                // const generator = this.arrayStringToArray(messageObject.generator)
+                const generator = messageObject.generator
+                this.addressBook[connection.peer] = {
+                    "dhObject":cryptojs.createDiffieHellman(prime,generator)
+                }
+                showMessage(`${this.peer.id} is generating its keys`)
+                
+                const myKeys = this.addressBook[connection.peer]["dhObject"].generateKeys()
+                
+                showMessage(`${this.peer.id} generated it's keys`)
+                showMessage(`${this.peer.id} is computing secret`)
+                
+                const mySecret = this.addressBook[connection.peer]["dhObject"].computeSecret(otherKeys)
+                
+                // showMessage(`${this.peer.id}'s secret is ${mySecret.toString()}`)
+                // console.log(`${this.peer.id}'s secret is ${mySecret.toString()}`)
+                this.addressBook[connection.peer] = {
+                    "secret":mySecret.toString()
+                }
+                
+                this.sendMessage(connection.peer,{
+                    "message_type":"join-fed-final-dh",
+                    "keys":myKeys.toJSON().data
+                })
+            }else if(messageObject.message_type==="join-fed-final-dh"){
+                // const otherKeys = this.arrayStringToArray(messageObject.keys)
+                const otherKeys = messageObject.keys
+                showMessage(`${this.peer.id} is computing its secret`)
+                const mySecret = this.addressBook[connection.peer]["dhObject"].computeSecret(otherKeys)
+                // showMessage(`${this.peer.id}'s secret is ${mySecret.toString()}`)
+                // console.log(`${this.peer.id}'s secret is ${mySecret.toString()}`)
+                this.addressBook[connection.peer] = {
+                    "secret":mySecret.toString()
+                }
             }
             else if(messageObject.message_type==="ping"){
                 this.sendMessage(connection.peer,{
                     "message-type":"ping",
                 })
             }
+            this.locked = false
+            console.log(`${this.peer.id}'s lock has been unlocked`)
         }
     }
 
